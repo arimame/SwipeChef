491477cd92e1423660d555cddbb72a9d08eb9349
Connor Peebles <connorpeebles93@gmail.com>
2018-11-15 17:08:16 -0500

    added SwipeCard example component

--- App.js
+++ App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Test from './test';
+import SwipeCards from './SwipeCards'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app! lol</Text>
-        <Test />
+      <View style={{flex:1}}>
+        <SwipeCards />
       </View>
     );
   }
